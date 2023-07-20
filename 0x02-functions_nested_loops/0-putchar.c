c3f67b857c7d9950f03036be2566c47933ec6008
DeTechgurl001 <aniakorkosarachi@gmail.com>
2023-07-20 16:02:17 -0700

    guts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
- *
- * Return: Always 1 (Success)
+ * Description: prints _putchar to standout 
+ * Return: Always 0
  */
 int main(void)
 {
-	putchar("_putchar \n");
-		return (0);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (1);
 }
