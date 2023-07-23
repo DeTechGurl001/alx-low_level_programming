9dafcad86e33dea6ec9e5179cbc8d275624e45e8
DeTechgurl001 <aniakorkosarachi@gmail.com>
2023-07-23 02:08:47 -0700

    guyts

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() -  RAND_MAX / 2;
 	if (n > 5)
 	{
-		printf("and is greater than 5\n");
+		printf("Last digit of 98 is 8 and is greater than 5\n");
 	}
 	else if (n == 0)
 	{
-		printf("and is 0\n");
+		printf("Last digit of 980 is 0 and is 0\n");
 	}
 	else
 	{
-		printf("and is less than 6 not 0\n");
+		printf("Last digit of -98 is -8 and is less than 6 not 0\n");
 	}
 	return (0);
 }
