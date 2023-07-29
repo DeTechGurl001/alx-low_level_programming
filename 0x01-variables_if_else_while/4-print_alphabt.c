590af72bd718750544eaef3fddff8daa5584613f
DeTechgurl001 <aniakorkosarachi@gmail.com>
2023-07-29 13:21:29 -0700

    huts

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char lowercase;
+	for (letter = 'a'; letter <= 'z'; letter++);
+	if  (letter != 'q' && letter != 'e');
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return 0;
+}
+
