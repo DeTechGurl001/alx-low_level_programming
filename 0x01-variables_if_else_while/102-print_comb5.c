914f875b32f03c789f0a70e9b46b876bf4485900
DeTechGurl001 <aniakorkosarachi@gmail.com>
2023-09-14 14:49:11 -0700

    guth

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,16 +17,16 @@ int main(void)
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar((' ');
-						putchar((j / 10) + 48);
-						putchar((j % 10) + 48);
-						if (i != 98 || j != 99)
-						{
-						putchar(',');
-						putchar(' ');
-						}
-						}
-						}
-						}
-						putchar('\n');
-						return (0);
-						}
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99);
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+				putchar('\n');
+				return (0);
+}
